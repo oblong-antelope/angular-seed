c1e235709436a71d365c7298d446db203bda7c55
Mickey Li <Mickey Li>
2016-10-17 18:42:18 +0100

    Modified fork so that the repo is now our project specific

--- tools/config/project.config.ts
+++ tools/config/project.config.ts
@@ -12,10 +12,10 @@ export class ProjectConfig extends SeedConfig {
 
   constructor() {
     super();
-    // this.APP_TITLE = 'Put name of your app here';
+    this.APP_TITLE = 'Oblong';
 
     /* Enable typeless compiler runs (faster) between typed compiler runs. */
-    // this.TYPED_COMPILE_INTERVAL = 5;
+    this.TYPED_COMPILE_INTERVAL = 5;
 
     // Add `NPM` third-party libraries to be injected/bundled.
     this.NPM_DEPENDENCIES = [
