b40b7698fe62c6c524455cdf4bcefd1d806ab4f0
Mickey Li <Mickeyhli@outlook.com>
2016-11-15 20:52:44 +0000

    Side bar controlling now working

--- src/client/app/home/home.component.ts
+++ src/client/app/home/home.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, ElementRef, ViewChild, Renderer, OnInit } from '@angular/core';
 
 /**
  * This class represents the lazy loaded home
@@ -11,6 +11,33 @@ import { Component } from '@angular/core';
   styleUrls: ['home.component.css'],
 })
 
-export class HomeComponent{
+export class HomeComponent implements OnInit {
 
+  home_route:boolean = true;
+  results_route:boolean = false;
+  profile_route:boolean = false;
+  right_open:boolean = true;
+
+  @ViewChild('leftContainer') left : ElementRef;
+  // @ViewChild('rightContainer') right : ElementRef;
+
+
+  constructor(private renderer:Renderer){}
+
+  ngOnInit() {
+    this.openRightIfChecked();
+  }
+
+  toggleRight() {
+    this.right_open = !this.right_open;
+    this.openRightIfChecked();
+  }
+
+  openRightIfChecked() {
+    if(this.right_open) {
+        this.renderer.setElementStyle(this.left.nativeElement, 'width', '60%');
+    } else {
+        this.renderer.setElementStyle(this.left.nativeElement, 'width', '100%');
+    }
+  }
 }
