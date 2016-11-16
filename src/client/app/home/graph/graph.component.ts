6434a8404623a7a7f98b39ea2cb31f1bedb2ac7b
Zichen Liu <liuzc@hotmail.co.uk>
2016-11-16 22:02:23 +0000

    added new charts

--- src/client/app/home/graph/graph.component.ts
+++ src/client/app/home/graph/graph.component.ts
@@ -2,7 +2,8 @@ import { Component, OnInit, ElementRef } from '@angular/core';
 import { Router } from '@angular/router';
 import { QueryService } from '../../shared/index';
 
-declare var Plotly:any;
+declare var $:any;
+declare var Highcharts:any;
 
 /**
  * This class represents the lazy loaded graphComponent.
@@ -38,103 +39,166 @@ export class GraphComponent implements OnInit{
   }
 
   plotlyPlot2() {
-    var TOTAL_ELEMENT_HEIGHT = this.elementRef.nativeElement.offsetHeight;
-    var TOTAL_ELEMENT_WIDTH = this.elementRef.nativeElement.offsetWidth;
-    var TOTAL_TITLE = 'AI vs AMMP vs GeoPhysics';
-    var NAME_LABEL_SIZE = 12;
-    var MARKER_DOT_SIZE = 12;
-    var T = 0;
+    $(function () {
 
-    Plotly.d3.csv('https://oblong-relentless.herokuapp.com/', function(err:any, rows:any){
-      setInterval(intervalWake, 200);
+      var t = 0;
 
-      var graphDiv = 'myPlotlyDiv';
+      $(document).ready(function(){
+        setInterval(runInter, 200);
+      });
 
-      function unpack(rows:any, key:any) {
-        return rows.map(function(row:any)
-        { return row[key]; });}
+      var dataa = [[1,2,3]];
+      //PRODUCTION: <=200; SAFE <=400; TESTING <=600; EXPERIMENTAL ~2000
+      for(var i=1; i<200; i++){
+        dataa[i] = [Math.random()*20-10, Math.random()*20-10, Math.random()*20-10]
+      }
 
-      var trace1 = {
-        x:unpack(rows, 'x'), y: unpack(rows, 'y'), z: unpack(rows, 'z'),
-        mode: 'markers',
-        marker: {
-          size: MARKER_DOT_SIZE,
-          line: {
-            color: 'rgba(217, 217, 217, 0.14)',
-            width: 1},
-          opacity: 0.8},
-        type: 'scatter3d',
-        text: unpack(rows, 'surname'),
-        textfont: {
-          size: NAME_LABEL_SIZE
-        },
-        hoverinfo: 'text'
-      };
+      function runInter(){
+        //chart.series[0].setData([[4*Math.sin(t)+3,5,4*Math.cos(t)+3],[4*Math.sin(t)-1,3,4*Math.cos(t)+1]], true);
+        chart.options.chart.options3d.beta = t%360;
+        chart.redraw();
+        t+=0.4;
+      }
 
-      var trace2 = {
-       x:unpack(rows, 'x2'), y: unpack(rows, 'y2'), z: unpack(rows, 'z2'),
-       mode: 'markers',
-       marker: {
-        color: 'rgb(127, 127, 127)',
-        size: MARKER_DOT_SIZE,
-        symbol: 'circle',
-        line: {
-       color: 'rgb(204, 204, 204)',
-       width: 1},
-       opacity: 0.8},
-       type: 'scatter3d'};
+      // Give the points a 3D feel by adding a radial gradient
+      Highcharts.getOptions().colors = $.map(Highcharts.getOptions().colors, function (color:any) {
+        return {
+          radialGradient: {
+            cx: 0.4,
+            cy: 0.3,
+            r: 0.5
+          },
+          stops: [
+            [0, color],
+            [1, Highcharts.Color(color).brighten(-0.2).get('rgb')]
+          ]
+        };
+      });
 
-      var data = [trace1,trace2];
-      var layout = {
-        margin: {
-          l: 0,
-          r: 0,
-          b: 0,
-          t: 0},
-        height: TOTAL_ELEMENT_HEIGHT,
-        width: TOTAL_ELEMENT_WIDTH,
-        scene:{
-          camera:{
-            center:{
-              x:0,
-              y:0,
-              z:0
-            },
-            eye:{//zoom of the thing
-              x:0,
-              y:2,
-              z:0.5
+      // Set up the chart
+      var chart = new Highcharts.Chart({
+        chart: {
+          animation: true,
+          renderTo: 'container',
+          margin: 10,
+          type: 'scatter',
+          options3d: {
+            enabled: true,
+            alpha: 10,
+            beta: -20,
+            depth: 200,
+            viewDistance: 3,
+            fitToPlot: false,
+            frame: {
+              bottom: { size: 1, color: 'rgba(0,0,0,0)' },
+              back: { size: 1, color: 'rgba(0,0,0,0)' },
+              side: { size: 1, color: 'rgba(0,0,0,0)' }
             }
+          }
+        },
+        title: {
+          text: ''
+        },
+        yAxis: {
+          min: -10,
+          max: 10,
+          title: null,
+          lineWidth: 0,
+          gridLineWidth: 0,
+          minorGridLineWidth: 0,
+          lineColor: 'transparent',
+          labels: {
+            enabled: false
           },
-          xaxis:{
-            showgrid:false,
-            zeroline:false,
-            showticklabels:false,
-            title:''
+          minorTickLength: 0,
+          tickLength: 0
+        },
+        xAxis: {
+          min: -10,
+          max: 10,
+          gridLineWidth: 0,
+          lineWidth: 0,
+          minorGridLineWidth: 0,
+          lineColor: 'transparent',
+          labels: {
+            enabled: false
           },
-          yaxis:{
-            showgrid:false,
-            zeroline:false,
-            showticklabels :false,
-            title:''
+          minorTickLength: 0,
+          tickLength: 0
+        },
+        zAxis: {
+          min: -10,
+          max: 10,
+          showFirstLabel: false,
+          gridLineWidth: 0,
+          lineWidth: 0,
+          minorGridLineWidth: 0,
+          lineColor: 'transparent',
+          labels: {
+            enabled: false
           },
-          zaxis:{
-            showgrid:false,
-            zeroline:false,
-            showticklabels:false,
-            title:''
+          minorTickLength: 0,
+          tickLength: 0
+        },
+        legend: {
+          enabled: false
+        },
+        series: [{
+          name: 'Reading',
+          colorByPoint: true,
+          data: dataa
+        }],
+        plotOptions: {
+          scatter: {
+            width: 100,
+            height: 100,
+            depth: 100
+          },
+          series: {
+            cursor: 'pointer',
+            point: {
+              events: {
+                click: function (e:any) {
+                  alert(this.index);
+                  //console.log(this);
+                }
+              }
+            }
           }
         }
-      };
-      Plotly.newPlot(graphDiv, data, layout);
+      });
 
-      function intervalWake(){
-        //Plotly.purge(graphDiv);
-        T+=0.03;
-        layout.scene.camera.eye.x = Math.sin(T)*2;
-        layout.scene.camera.eye.y = Math.cos(T)*2;
-        Plotly.newPlot(graphDiv, data, layout);
-      }
+
+      // Add mouse events for rotation
+      $(chart.container).on('mousedown.hc touchstart.hc', function (eStart:any) {
+        eStart = chart.pointer.normalize(eStart);
+
+        var posX = eStart.pageX,
+          posY = eStart.pageY,
+          alpha = chart.options.chart.options3d.alpha,
+          beta = chart.options.chart.options3d.beta,
+          newAlpha:any,
+          newBeta:any,
+          sensitivity = 5; // lower is more sensitive
+
+        $(document).on({
+          'mousemove.hc touchdrag.hc': function (e:any) {
+            // Run beta
+            newBeta = beta + (posX - e.pageX) / sensitivity;
+            chart.options.chart.options3d.beta = newBeta;
+
+            // Run alpha
+            newAlpha = alpha + (e.pageY - posY) / sensitivity;
+            chart.options.chart.options3d.alpha = newAlpha;
+
+            chart.redraw(false);
+          },
+          'mouseup touchend': function () {
+            $(document).off('.hc');
+          }
+        });
+      });
+
     });
   }
 
