e531da47967360bf8b2ddbc7f0460b1d332a4c19
Mickey Li <Mickeyhli@outlook.com>
2017-01-11 16:38:56 +0000

    Now on https

--- tools/env/dev.ts
+++ tools/env/dev.ts
@@ -1,7 +1,7 @@
 import { EnvConfig } from './env-config.interface';
 
 const DevConfig: EnvConfig = {
-  BACKEND_API: 'http://battle.horse/oblong',
+  BACKEND_API: 'https://battle.horse/oblong',
   VIS_HOME_API: 'https://oblong-relentless.herokuapp.com',
   VIS_SEARCH_API: 'https://oblong-kittens.herokuapp.com',
   VIS_PROFILE_API: 'https://oblong-onslaught.herokuapp.com',
